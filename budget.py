c9dc31a79fcfedd4919d70059840f69bd854ffde
santiagoroad <santiagoroa5@gmail.com>
2023-12-11 17:32:02 -0500

    Method transfer and how to print the information of the class

--- budget.py
+++ budget.py
@@ -3,14 +3,14 @@ class Category:
     self.category = category
     self.ledger = []
 
-  def getbalance(self):
+  def get_balance(self):
     balance = 0
     for i in self.ledger:
       balance += i["amount"]
     return balance
 
   def check_funds(self, amount):
-    if amount > self.getbalance():
+    if amount > self.get_balance():
       return False
     else:
       return True
@@ -24,5 +24,22 @@ class Category:
       return True
     else:
       return False
+  
+  def transfer(self, amount, category):
+    if self.check_funds(amount):
+      self.withdraw(amount, f"Transfer to {category.category}")
+      category.deposit(amount, f"Transfer from {self.category}")
+      return True
+    else:
+      return False
 
-# def create_spend_chart(categories):
+  def __str__(self):
+    title = f"{self.category.center(30, '*')}\n"
+    items = ""
+    for i in self.ledger:
+      items += f"{i['description'][:23].ljust(23)} {format(i['amount'],'.2f').rjust(7)}\n"
+    total = f"Total: {format(self.get_balance(), '.2f')}"
+    return title + items + total
+
+def create_spend_chart(categories):
+  print(categories)
