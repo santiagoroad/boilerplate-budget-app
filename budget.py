aa32a4b42bb538c3fd729fc1117fcceb07bfb534
Replit user <>
2023-12-11 21:14:26 +0000

    Methods to the budget

--- budget.py
+++ budget.py
@@ -1,7 +1,28 @@
 class Category:
+  def __init__(self, category):
+    self.category = category
+    self.ledger = []
 
+  def getbalance(self):
+    balance = 0
+    for i in self.ledger:
+      balance += i["amount"]
+    return balance
 
+  def check_funds(self, amount):
+    if amount > self.getbalance():
+      return False
+    else:
+      return True
 
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
 
+  def withdraw(self, amount, description=""):
+    if self.check_funds(amount):
+      self.ledger.append({"amount": -amount, "description": description})
+      return True
+    else:
+      return False
 
-def create_spend_chart(categories):
+# def create_spend_chart(categories):
